fdc047b20f937c4a9bcb24e3858dfcf9dad745e6
Erik Jacobson <erik@jacobsonfarm.com>
2019-05-03 15:42:42 -0500

    Initial Commit

---
+++ BlogPost.py
@@ -0,0 +1,51 @@
+class BlogPost:
+    def __init__(self, entry):
+        self._id = entry['id']
+        self._title = entry['title']
+        self._link = entry['link']
+        self._content = entry['summary']
+        self._published = entry['published']
+        self._medium = False
+        self._instagram = False
+    
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def link(self):
+        return self._link
+
+    @property
+    def content(self):
+        return self._content
+    
+    @property
+    def published(self):
+        return self._published
+
+    @property
+    def medium(self):
+        return self._medium
+    
+    @property
+    def instagram(self):
+        return self._instagram
+    
+    @property
+    def data(self):
+        data = [self._id,self._title,self._link, self._content, self._published, self._medium, self._instagram]
+        df = pd.DataFrame([data], columns=['id','title','link','content','published','medium','instagram'])
+        return df
+
+    @medium.setter
+    def medium(self, n):
+        self._medium = n
+    
+    @instagram.setter
+    def instagram(self, n):
+        self._instagram = n
---
+++ main.py
@@ -0,0 +1,67 @@
+import json
+import feedparser
+import requests
+import numpy as np
+import pandas as pd
+import BlogPost
+
+def main():
+    # Link to RSS Feed
+    RSS = 'https://www.erikwjacobson.com/blog?format=rss'
+    
+    # Load Credentials
+    f = open('credentials.json', 'r')
+    creds = json.loads(f.read())
+    f.close()
+    
+    # For each post
+    posts = feedparser.parse(RSS)
+    for post in posts['entries']:
+        post = BlogPost(post) # Instantiate
+        
+        # If the post has not been posted to medium yet
+        if(not posted(post, 'medium')):
+            # Post to medium
+            success = postToMedium(creds, post)
+            if(success):
+                post.medium = True
+                logPost(post) # Log the post
+                print('Successfully posted "{}" to Medium'.format(post.title))
+            else:
+                print('Something went wrong with the request!')
+    print('Done')
+
+## Check if posted
+def posted(post, where):
+    logs = pd.read_csv('logs.csv')
+    postLog = logs[logs['id'] == post.id]
+    if(postLog.empty):
+        posted = False
+    else:
+        posted = postLog.iloc[0][where]
+    return posted
+
+## Log the post to a csv file
+def logPost(post):
+    post.data.to_csv('logs.csv', mode='a', header=False)
+    print('Successfully logged post.')
+
+## Post to medium
+def postToMedium(creds, post):
+    id = creds['medium_author_id']
+    url = "https://api.medium.com/v1/users/{}/posts".format(id)
+    data = ({
+        'title': post.title,
+        'contentFormat': "html",
+        "content": post.content
+    })
+    headers = ({
+        'Authorization': 'Bearer {}'.format(creds['medium_token']),
+        'Content-Type': 'application/json'
+    })
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    print(r.json())
+    posted = r.status_code == 201 # OK Status
+    return posted
+
+main()
